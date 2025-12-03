f81681ab9bc8e2a03d9313b33a719b91dba12293
Cleber Silva <cleber_ss26@hotmail.com>
2025-12-03 17:45:31 -0300

    criando pacote modificador final

---
+++ src/academy/devcsilva/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package academy.devcsilva.maratonajava.javacore.Jmodificadorfinal.test;
+
+import academy.devcsilva.maratonajava.javacore.Jmodificadorfinal.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
