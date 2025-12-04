ba0271e35de9ba1dfc3f224de40763e4c30d0824
Cleber Silva <cleber_ss26@hotmail.com>
2025-12-04 16:22:49 -0300

    criando a classe Comprador

--- src/academy/devcsilva/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devcsilva/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,10 +1,15 @@
 package academy.devcsilva.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devcsilva.maratonajava.javacore.Jmodificadorfinal.domain.Carro;
+import academy.devcsilva.maratonajava.javacore.Jmodificadorfinal.domain.Comprador;
 
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Kuririn");
+        System.out.println(carro.COMPRADOR);
     }
 }
