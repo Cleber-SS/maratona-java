07b3dc5ed9e62b82c31b4db38c0716396e232180
Cleber Silva <cleber_ss26@hotmail.com>
2025-12-15 16:45:02 -0300

    arualizando a classes interfaces

--- src/academy/devcsilva/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/academy/devcsilva/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -1,5 +1,6 @@
 package academy.devcsilva.maratonajava.javacore.Minterfaces.test;
 
+import academy.devcsilva.maratonajava.javacore.Minterfaces.domain.DataLoader;
 import academy.devcsilva.maratonajava.javacore.Minterfaces.domain.DatabaseLoader;
 import academy.devcsilva.maratonajava.javacore.Minterfaces.domain.FileLoader;
 
@@ -7,7 +8,17 @@ public class DataLoaderTest01 {
     public static void main(String[] args) {
         DatabaseLoader databaseLoader = new DatabaseLoader();
         FileLoader fileLoader = new FileLoader();
+
         databaseLoader.load();
         fileLoader.load();
+
+        databaseLoader.remove();
+        fileLoader.remove();
+
+        databaseLoader.checkPermission();
+        fileLoader.checkPermission();
+
+        DataLoader.retrieveMaxSize();
+        DatabaseLoader.retrieveMaxSize();
     }
 }
