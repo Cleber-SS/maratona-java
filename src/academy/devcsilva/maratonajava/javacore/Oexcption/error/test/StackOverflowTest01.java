be34199f3a075c943843e6772523f07b89b03ff7
Cleber Silva <cleber_ss26@hotmail.com>
2025-12-26 16:49:27 -0300

    criando o pacote exception

---
+++ src/academy/devcsilva/maratonajava/javacore/Oexcption/error/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devcsilva.maratonajava.javacore.Oexcption.error.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
