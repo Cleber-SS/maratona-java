8b92b39b0a50297c79ed4ae59c6db01a252efc89
Cleber Silva <cleber_ss26@hotmail.com>
2026-01-26 15:33:32 -0300

    Adicionando a classe ChronoUnit

---
+++ src/academy/devcsilva/maratonajava/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,16 @@
+package academy.devcsilva.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(1987, Month.FEBRUARY, 26, 4, 35);
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
