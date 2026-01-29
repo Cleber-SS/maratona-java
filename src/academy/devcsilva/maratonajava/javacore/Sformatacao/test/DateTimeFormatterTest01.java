2e1cef7c1d810e9f5201775aa90146006677a952
Cleber Silva <cleber_ss26@hotmail.com>
2026-01-29 17:01:34 -0300

    Adicionando a classe DateTimeFormatter

---
+++ src/academy/devcsilva/maratonajava/javacore/Sformatacao/test/DateTimeFormatterTest01.java
@@ -0,0 +1,45 @@
+package academy.devcsilva.maratonajava.javacore.Sformatacao.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        LocalDate parse1 = LocalDate.parse("20260129", DateTimeFormatter.BASIC_ISO_DATE);
+        LocalDate parse2 = LocalDate.parse("2026-01-29+05:00", DateTimeFormatter.ISO_DATE);
+        LocalDate parse3 = LocalDate.parse("2026-01-29", DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(parse1);
+        System.out.println(parse2);
+        System.out.println(parse3);
+
+        LocalDateTime now = LocalDateTime.now();
+        String s4 = now.format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(s4);
+        LocalDateTime parse4 = LocalDateTime.parse("2026-01-29T16:45:33.682609", DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(parse4);
+
+        //dia/MM/yyyy
+        //MM/dd/yyyy
+        //yyyy/MM/dd
+        DateTimeFormatter formatterBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatBR = LocalDate.now().format(formatterBR);
+        System.out.println(formatBR);
+        LocalDate parseBR = LocalDate.parse("29/01/2026", formatterBR);
+        System.out.println(parseBR);
+
+        DateTimeFormatter formatterGR = DateTimeFormatter.ofPattern("dd.MMMM.yyyy", Locale.GERMAN);
+        String formatGR = LocalDate.now().format(formatterGR);
+        System.out.println(formatGR);
+        LocalDate parseGR = LocalDate.parse("29.Januar.2026", formatterGR);
+        System.out.println(parseGR);
+    }
+}
