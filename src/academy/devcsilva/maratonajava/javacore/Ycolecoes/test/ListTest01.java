22511a075bd10e663c98d356b0467756c0b9024c
Cleber Silva <cleber_ss26@hotmail.com>
2026-03-10 16:18:20 -0300

    Adicionando a classe List

---
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -0,0 +1,22 @@
+package academy.devcsilva.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList<>(16);
+        nomes.add("Cleber");
+        nomes.add("Dev Academy");
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+        nomes.add("Silva");
+        System.out.println("---------------");
+
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+    }
+}
