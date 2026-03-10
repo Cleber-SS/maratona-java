db1d05bb762ac1c0f3f11bb4429a5ad66a863eea
Cleber Silva <cleber_ss26@hotmail.com>
2026-03-10 16:40:52 -0300

    Ajustando a classe List

--- src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,17 +6,26 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Cleber");
         nomes.add("Dev Academy");
+        nomes2.add("Silva");
+        nomes2.add("Academy");
+
+//        System.out.println(nomes.remove("cleber"));
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
         }
-        nomes.add("Silva");
+
         System.out.println("---------------");
 
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
