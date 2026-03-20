0fba7d25ebd0f0ee6f30f6e19b0a5cf4606fea5f
Cleber Silva <cleber_ss26@hotmail.com>
2026-03-20 15:47:13 -0300

    Ajustando a classe Manga utilizando a classe Iterator

--- src/academy/devcsilva/maratonajava/javacore/Ycolecoes/domain/Manga.java
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/domain/Manga.java
@@ -6,6 +6,7 @@ public class Manga implements Comparable<Manga> {
     private Long id;
     private String nome;
     private double preco;
+    private int quantidade;
 
     public Manga(Long id, String nome, double preco) {
         Objects.requireNonNull(id, "Id não pode ser null");
@@ -13,6 +14,11 @@ public class Manga implements Comparable<Manga> {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public Manga(Long id, String nome, double preco, int quantidade) {
+        this(id, nome, preco);
+        this.quantidade = quantidade;
     }
 
     @Override
@@ -34,6 +40,7 @@ public class Manga implements Comparable<Manga> {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", preco=" + preco +
+                ", quantidade=" + quantidade +
                 '}';
     }
 
@@ -59,6 +66,14 @@ public class Manga implements Comparable<Manga> {
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 
     @Override
---
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
@@ -0,0 +1,29 @@
+package academy.devcsilva.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devcsilva.maratonajava.javacore.Ycolecoes.domain.Manga;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorTest01 {
+    public static void main(String[] args) {
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
+        mangas.add(new Manga(1L, "Berserk", 9.5, 5));
+        mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
+        mangas.add(new Manga(3L, "Attack on Titan", 11.20, 2));
+        mangas.add(new Manga(2L, "Dragon Ball Z", 2.99, 0));
+
+//        Iterator<Manga> mangaIterator = mangas.iterator();
+//        while (mangaIterator.hasNext()) {
+//            if (mangaIterator.next().getQuantidade() == 0) {
+//                mangaIterator.remove();
+//            }
+//        }
+
+        mangas.removeIf(manga -> manga.getQuantidade() == 0);
+
+        System.out.println(mangas);
+    }
+}
