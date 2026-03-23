e3301650240df1d8a8e1f46bd1bd6aa1b50d9d21
Cleber Silva <cleber_ss26@hotmail.com>
2026-03-23 15:50:00 -0300

    Ajustando a classe NavigableSet com TreeSet

--- src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -15,12 +15,20 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone> {
     }
 }
 
+class MangaPrecoComparator implements Comparator<Manga> {
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L, "Hellsing Ultimate", 19.9, 0));
         mangas.add(new Manga(1L, "Berserk", 9.5, 5));
         mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
@@ -31,5 +39,20 @@ public class NavigableSetTest01 {
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
+
+        Manga yuyu = new Manga(21L, "Yuyu Hakusho", 3.2, 5);
+        //métodos para preço:
+        //lower <, floor <=, higher > e o ceiling >=.
+        System.out.println("--------------------");
+        System.out.println(mangas.lower(yuyu));
+        System.out.println(mangas.floor(yuyu));
+        System.out.println(mangas.higher(yuyu));
+        System.out.println(mangas.ceiling(yuyu));
+
+        System.out.println("---------------------");
+        System.out.println(mangas.size());
+//        System.out.println(mangas.pollFirst());
+        System.out.println(mangas.pollLast());
+        System.out.println(mangas.size());
     }
 }
