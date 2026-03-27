b6393c502aa0833b7a4d2a4dd4eda0b33549eea3
Cleber Silva <cleber_ss26@hotmail.com>
2026-03-27 16:13:11 -0300

    Atualizando a classe Map, HashMao, LinkedHashMap

---
+++ src/academy/devcsilva/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,27 @@
+package academy.devcsilva.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devcsilva.maratonajava.javacore.Ycolecoes.domain.Consumidor;
+import academy.devcsilva.maratonajava.javacore.Ycolecoes.domain.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Binho Santos");
+        Consumidor consumidor2 = new Consumidor("Dev Academy");
+
+        Manga manga1 = new Manga(5L, "Hellsing Ultimate", 19.9);
+        Manga manga2 = new Manga(1L, "Berserk", 9.5);
+        Manga manga3 = new Manga(4L, "Pokemon", 3.2);
+        Manga manga4 = new Manga(3L, "Attack on Titan", 11.20);
+        Manga manga5 = new Manga(2L, "Dragon Ball Z", 2.99);
+
+        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
+        consumidorManga.put(consumidor1, manga1);
+        consumidorManga.put(consumidor2, manga4);
+        for (Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()) {
+            System.out.println(entry.getKey().getNome() + " - " + entry.getValue().getNome());
+        }
+    }
+}
