07fbd7b1e8232a9d7665db92cc2e4b5e4e12122a
Cleber Silva <cleber_ss26@hotmail.com>
2026-04-07 15:53:02 -0300

    Atualizando a classe Generics

---
+++ src/academy/devcsilva/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,28 @@
+package academy.devcsilva.maratonajava.javacore.Zgenerics.test;
+
+import academy.devcsilva.maratonajava.javacore.Zgenerics.domain.Barco;
+import academy.devcsilva.maratonajava.javacore.Zgenerics.domain.Carro;
+import academy.devcsilva.maratonajava.javacore.Zgenerics.service.BarcoRentavelService;
+import academy.devcsilva.maratonajava.javacore.Zgenerics.service.RentalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseGenericaTest03 {
+    public static void main(String[] args) {
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Fusca")));
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Canoa")));
+        RentalService<Carro> rentalServiceCarro = new RentalService<>(carrosDisponiveis);
+        Carro carro = rentalServiceCarro.buscarObjetoDisponivel();
+        System.out.println("Usando o carro por um mês...");
+        rentalServiceCarro.retornarObjetoAlugado(carro);
+
+        System.out.println("---------------------------------------");
+
+        RentalService<Barco> rentalServiceBarco = new RentalService<>(barcosDisponiveis);
+        Barco barco = rentalServiceBarco.buscarObjetoDisponivel();
+        System.out.println("Usando o barco por um mês...");
+        rentalServiceBarco.retornarObjetoAlugado(barco);
+
+    }
+}
