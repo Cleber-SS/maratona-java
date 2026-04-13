0c3cda29c3597f855b2d740570f1032c3489334f
Cleber Silva <cleber_ss26@hotmail.com>
2026-04-13 16:01:49 -0300

    Ajustando a classe Parametrizando comportamentos

---
+++ src/academy/devcsilva/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package academy.devcsilva.maratonajava.javacore.ZZBcomportamento.interfaces;
+
+import academy.devcsilva.maratonajava.javacore.ZZBcomportamento.domain.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
